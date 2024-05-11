a7a18e62d9c32013a5cde4f74eac7fb3c6765860
findsky6544 <36687288+findsky6544@users.noreply.github.com>
2024-05-11 20:30:03 +0800

    增加了一些指令
    增加了.help/.en/,r的指令
    但是有些复杂的.r的指令还无法识别，有时间再改改吧

---
+++ src/wechaty/config.js
@@ -0,0 +1,49 @@
+// 定义机器人的名称，这里是为了防止群聊消息太多，所以只有艾特机器人才会回复，
+// 这里不要把@去掉，在@后面加上你启动机器人账号的微信名称
+export const botName = '@荣顶'
+
+// 群聊白名单，白名单内的群聊才会自动回复
+export const roomWhiteList = ['群名称', '前端超人⑤【WeChat-Bot】']
+
+// 联系人白名单，白名单内的联系人才会自动回复
+export const aliasWhiteList = ['备注名或微信名', '张三', '李四']
+
+export const help = {
+    'default':
+        `——微信骰娘使用指南——
+已经开发的功能有：
+.coc [n]：生成n张COC人物卡（默认为1）
+.en <技能名称> (技能点数)：成长检定
+.help：显示本页面
+.r [掷骰表达式] ([掷骰原因])：投掷骰子
+.rh：暗骰1D100
+.sc：理智检定
+备注：
+[]内为选填，<>内为必填；填写命令时不需要写括号。
+有些指令有单独的帮助页面，输入.help+该项目（如.help r）查看帮助。`,
+    'r':
+        `——R功能使用指南——
+.r [掷骰表达式] [掷骰原因]
+[掷骰表达式]：([掷骰次数]#)[骰子个数]d[骰子面数](b[奖励骰个数])(p[惩罚骰个数])(k[取点数最大的骰子数])
+默认为骰1D100`,
+    'sc':
+        `——SC功能使用指南——
+.sc <x /y>：理智检定，若成功则减少x点理智，失败则减少y点理智`,
+    'en':
+        `——EN功能使用指南——
+.en <技能名称> (技能点数)：骰D100，若点数大于技能点数，属性成长1d10`
+}
+
+export function formatNewPlayerTips(name) { return `${name}，这是新的调查员数据：` }
+export function formatErrTips() { return `格式错误，请检查` }
+export function formatNewPropertyTips(property) { return `\n力量STR：${property[0]}，体质CON：${property[1]}，体型SIZ：${property[2]}，敏捷DEX：${property[3]}，外貌APP：${property[4]}，智力INT：${property[5]}，意志POW：${property[6]}，教育EDU：${property[7]}，幸运LUK：${property[8]}，总和(不含幸运)SUM：${property[9]}(${property[10]})` }
+export function formaEnSuccessTips(name, skill,target, result, addValue) {
+    return `${name}的“${skill}”成长检定：
+D100=${result}/${target} 成功
+“${skill}”增加了1d10=${addValue}点，当前为${parseInt(target) + parseInt(addValue)}点`
+}
+export function formaEnFailedTips(name, skill, target, result) {
+    return `${name}的“${skill}”成长检定：
+D100=${result}/${target} 失败
+“${skill}”成长失败了！`
+}
--- src/wechaty/sendMessage.js
+++ src/wechaty/sendMessage.js
@@ -1,4 +1,4 @@
-import { botName, roomWhiteList, aliasWhiteList } from '../../config.js'
+import * as config from './config.js'
 
 /**
  * 默认消息发送
@@ -16,30 +16,37 @@ export async function defaultMessage(msg, bot) {
     const remarkName = await contact.alias() // 备注名称
     const name = await contact.name() // 微信名称
     const isText = msg.type() === bot.Message.Type.Text // 消息类型是否为文本
-    const isRoomWhite = roomWhiteList.includes(roomName) // 是否在群聊白名单内
-    const isAt = content.includes(`${botName}`) //艾特了机器人
-    const isAlias = aliasWhiteList.includes(remarkName) || aliasWhiteList.includes(name) // 发消息的人是否在联系人白名单内
-    const isBotSelf = botName === remarkName || botName === name // 是否是机器人自己
+    const isRoomWhite = config.roomWhiteList.includes(roomName) // 是否在群聊白名单内
+    const isAt = content.includes(`${config.botName}`) //艾特了机器人
+    const isAlias = config.aliasWhiteList.includes(remarkName) || config.aliasWhiteList.includes(name) // 发消息的人是否在联系人白名单内
+    const isBotSelf = config.botName === remarkName || config.botName === name // 是否是机器人自己
     // TODO 你们可以根据自己的需求修改这里的逻辑
     if (isBotSelf || !isText) return // 如果是机器人自己发送的消息或者消息类型不是文本则不处理
     try {
         // 区分群聊和私聊
         if (room) {
-            let content =msg.text();
+            let content = msg.text();
             if (isAt) {
                 content = await msg.mentionText() || content.replace(`${botName}`, '') // 去掉艾特的消息主体
             }
-            console.log(name + ': ', content)
+            console.log(roomName + ":" + name + ': ', content)
             const response = await getReply(name, content)
-            console.log(response)
-            await room.say(response)
+            if (response != '') {
+                const date = new Date()
+                console.log(date + ":" + response)
+                await room.say(response)
+            }
         }
         // 私人聊天，白名单内的直接发送
         if (!room) {
             console.log(name + ': ', content)
             const response = await getReply(name, content)
-            console.log(response)
-            await contact.say(response)
+
+            if (response != '') {
+                const date = new Date()
+                console.log(date + ":" + response)
+                await contact.say(response)
+            }
         }
     } catch (e) {
         console.error(e)
@@ -49,13 +56,16 @@ export async function defaultMessage(msg, bot) {
 
 async function getReply(name, text) {
     var str = ''
-    if (text.slice(0, 4) == '.coc') {
-        str = `${name}，这是新的调查员数据：`
+    if (text.startsWith('.coc')) {
+        var order = text.slice(4).trim()
+
+        str = config.formatNewPlayerTips(name)
         var count = 1;
-        if (text.length > 4) {
-            count = Number(text.slice(4).trim());
+
+        if (order.length > 0) {
+            count = Number(order);
             if (!Number.isInteger(count)) {
-                return '格式错误，请输入.coc [数字]';
+                str = config.formatErrTips()
             }
         }
         var property = [11]
@@ -73,13 +83,202 @@ async function getReply(name, text) {
             property[9] = property[0] + property[1] + property[2] + property[3] + property[4] + property[5] + property[6] + property[7];
             property[10] = property[0] + property[1] + property[2] + property[3] + property[4] + property[5] + property[6] + property[7] + property[8];
             str += '\n----------------------------------------'
-            str += '\n力量STR：' + property[0] + '，体质CON：' + property[1] + '，体型SIZ：' + property[2] + '，敏捷DEX：' + property[3] + '，外貌APP：' + property[4] + '，智力INT：' + property[5] + '，意志POW：' + property[6] + '，教育EDU：' + property[7] + '，幸运LUK：' + property[8] + '，总和(不含幸运)SUM：' + property[9] + '(' + property[10] + ')'
+            str += config.formatNewPropertyTips(property)
 
+        }
+    }
+    else if (text.startsWith(".en")) {
+        var order = text.slice(3).trim()
+        var array = order.split(' ')
+        if (array.length == 2 && Number.isInteger(Number(array[1]))) {
+            var result = getRandomInt(100);
+            if (result > array[1]) {
+                var addValue = getRandomInt(10)
+                str = config.formaEnSuccessTips(name, array[0], array[1], result, addValue)
+            }
+            else {
+                str = config.formaEnFailedTips(name, array[0], array[1], result)
+            }
+        }
+        else {
+            str = config.formatErrTips()
+        }
+    }
+    else if (text.startsWith(".help")) {
+        var order = text.slice(5).trim()
+
+        if (order.length == 0) {
+            str = config.help["default"];
+        }
+        else if (order in config.help) {
+            str = config.help[order];
+        }
+        else {
+            str = config.formatErrTips()
+        }
+    }
+    else if (text.startsWith(".r")) {
+        var order = text.slice(2).trim()
+        if (order.length > 0) {
+            var funcReg = /^(((\d+d\d+)|\d+)#)?((\d*d\d*)([\*xX\+](((\d*d\d*)|\d*)#)?((\d*d\d*)|\d*))*|([bp]\d*))$/
+
+            var firstBlankIndex = order.indexOf(' ')
+            if (firstBlankIndex == -1) {
+                if (funcReg.test(order)) {
+                    str = `${name} 掷出了${getRResult(order)}`
+                }
+                else {
+                    str = `由于${order}，${name}掷出了${getRResult('d100')}`
+                }
+            }
+            else {
+                var diceFunc = order.slice(0, firstBlankIndex);
+                var reason = order.slice(firstBlankIndex+1)
+                str = `由于${reason}，${name}掷出了${getRResult(diceFunc)}`
+            }
+        }
+        else {
+            str = `${name} 掷出了${getRResult('d100')}`
         }
     }
     return str;
 }
 
-function getRandomInt(max){
-	return Math.floor(Math.random() * max)+1;
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function getRResult(rStr) {
+    var result = ''
+    if (rStr.includes('b') && rStr.includes('p')) {
+        result = 'b和p不能同时使用'
+    }
+    else if ((rStr.match(/b\d*/) != null && rStr.match(/b\d*/).length > 1) || (rStr.match(/p\d*/) != null && rStr.match(/p\d*/).length > 1)) {
+        result = config.formatErrTips()
+    }
+    else {
+        var baseReg = /\d+d\d+/
+
+        var runTime = 1
+
+        var sharpReg = /((\d+d\d+)|\d+)#/
+        if (sharpReg.test(rStr)) {
+            var sharpStr = rStr.match(sharpReg)[0].replace('#', '')
+            if (baseReg.test(sharpStr)) {
+                var r = getDiceResult(sharpStr)
+                runTime = r.value
+            }
+            else {
+                runTime = Number(sharpStr)
+            }
+            result = `掷骰${runTime}次：`
+            for (var i = 0; i < runTime; i++) {
+                var diceFunc = rStr.replace(sharpReg, '')
+                var diceResult = getDiceResult(diceFunc)
+                result += `\n${diceFunc}=${diceResult.detail}=${diceResult.value}`
+            }
+        }
+        else {
+            var diceResult = getDiceResult(rStr)
+            if (/\[.*\]/.test(diceResult.detail)) {
+                result = `${rStr}=${diceResult.detail}=${diceResult.value}`
+            }
+            else {
+                result = `${rStr}=${diceResult.value}`
+            }
+        }
+    }
+    if (/d=/.test(result)) {
+        result = result.replace('d', 'd100')
+    }
+    return result
+}
+
+function getDiceResult(diceFunc) {
+    var result = { "detail": '', "value": 0 }
+    if (diceFunc.includes('b')) {
+        result.detail += '奖励'
+
+        var bCount = 1
+        if (/b\d+/.test(diceFunc )) {
+            bCount = Number(diceFunc.match(/b\d+/)[0].replace('b', ''))
+        }
+        var baseResult = getDiceResult('d100')
+
+        for (var i = 0; i < bCount; i++) {
+            var bValue = getDiceResult('1d10')
+            bValue.value -= 1
+            result.detail += ' ' + bValue.value
+            if (baseResult.value / 10 > bValue.value) {
+                baseResult.value = bValue.value * 10 + baseResult.value % 10
+            }
+        }
+        result.value = baseResult.value
+        result.detail = `[${baseResult.detail},${result.detail}]`
+    }
+    else if (diceFunc.includes('p')) {
+        result.detail += '惩罚'
+
+        var pCount = 1
+        if (/p\d+/.test(diceFunc)) {
+            pCount = Number(diceFunc.match(/p\d+/)[0].replace('p', ''))
+        }
+        var baseResult = getDiceResult('d100')
+
+        for (var i = 0; i < pCount; i++) {
+            var pValue = getDiceResult('1d10')
+            pValue.value -= 1
+            result.detail += ' ' + pValue.value
+            if (baseResult.value / 10 < pValue.value) {
+                baseResult.value = pValue.value * 10 + baseResult.value % 10
+            }
+        }
+        result.value = baseResult.value
+        result.detail = `[${baseResult.detail},${result.detail}]`
+    }
+    else if (diceFunc.includes('+')) {
+        var multiValue = Number(diceFunc.match(/\+\d+/)[0].replace(/\+/, ''))
+        var currentResult = getDiceResult(diceFunc.replace(/\+\d+/, ''));
+        if (!/\d+\[/.test(currentResult.detail)) {
+            currentResult.detail = currentResult.value+currentResult.detail
+        }
+        result.detail = `${currentResult.detail} + ${multiValue}`
+        result.value = currentResult.value + multiValue
+    }
+    else if (/[*xX]/.test(diceFunc )) {
+        var multiValue = Number(diceFunc.match(/[*xX]\d+/)[0].replace(/[*xX]/, ''))
+        var currentResult = getDiceResult(diceFunc.replace(/[*xX]\d+/, ''));
+        if (!/\d+\[/.test(currentResult.detail)) {
+            currentResult.detail = currentResult.value+currentResult.detail
+        }
+        result.detail = `${currentResult.detail} * ${multiValue}`
+        result.value = currentResult.value * multiValue
+    }
+    else if (/^\d*d\d*$/.test(diceFunc )) {
+        var runTime = 1;
+        var diceMax = 100;
+
+        if (/\d+d/.test(diceFunc)) {
+            runTime = diceFunc.match(/\d+d/)[0].replace('d', '');
+        }
+        if (/d\d+/.test(diceFunc )) {
+            diceMax = diceFunc.match(/d\d+/)[0].replace('d', '');
+        }
+
+        var sum = 0;
+        for (var i = 0; i < runTime; i++) {
+            var currentValue = getRandomInt(diceMax);
+            result.detail += `+${currentValue}`
+            sum += currentValue
+        }
+        if (runTime > 1) {
+            result.detail = `[${result.detail.slice(1)}]`;
+        }
+        else {
+            result.detail = `${diceFunc}=${sum}`;
+        }
+
+        result.value = sum
+    }
+    return result
 }
